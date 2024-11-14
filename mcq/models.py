3d2067742139b7d0c3e6cc1d3cd97b65ead5186b
Monowar Hossain Khan <mhkha2022@gmail.com>
2024-11-14 21:58:37 +0600

    Question and options model

--- mcq/models.py
+++ mcq/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    question = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        db_table = 'mcq_questions'
+
+
+class QuestionOption(models.Model):
+    question = models.ForeignKey(
+        to=Question,
+        on_delete=models.CASCADE,
+        related_name='options'
+    )
+    option = models.TextField()
+    is_correct = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.option
+
+    class Meta:
+        db_table = 'mcq_question_options'
